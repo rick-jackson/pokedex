e9d526b6e8ee11beaffd669717965149baba833a
rick-jackson <vitaliisekreta@gmail.com>
2023-10-24 17:49:15 +0300

    added favicon

--- src/components/pokedex/PokemonsList/index.tsx
+++ src/components/pokedex/PokemonsList/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, CircularProgress } from "@mui/material";
+import { Button, CircularProgress, Typography } from "@mui/material";
 
 import type { Pokemon } from "../../../entities/pokemon";
 import useDataFetching from "../../../common/hooks/useDataFetching";
@@ -24,36 +24,39 @@ const PokemonsList: React.FC<PokemonsListProps> = ({
 
   if (error) return <Error error={error} />;
 
+  const pokemons = data
+    .filter((pokemon) => {
+      if (!pokemon?.id || !filterType) return pokemon;
+      return pokemon.types?.map(({ type }) => type.name).includes(filterType);
+    })
+    .map((pokemon) => {
+      if (loading && !pokemon?.id)
+        return <PokemonCardSkeleton key={pokemon.url} />;
+      return (
+        <PokemonCard
+          data={pokemon as Pokemon}
+          setActivePokemon={setActivePokemon}
+          activePokemon={activePokemon}
+          key={pokemon.id}
+        />
+      );
+    });
+
   return (
     <Styled.Container>
       <Toolbar loading={loading} setFilterType={setFilterType} />
       <Styled.PokemonsList>
-        {data
-          .filter((pokemon) => {
-            if (!pokemon?.id || !filterType) return pokemon;
-            return pokemon.types
-              ?.map(({ type }) => type.name)
-              .includes(filterType);
-          })
-          .map((pokemon) => {
-            if (loading && !pokemon?.id)
-              return <PokemonCardSkeleton key={pokemon.url} />;
-            return (
-              <PokemonCard
-                data={pokemon as Pokemon}
-                setActivePokemon={setActivePokemon}
-                activePokemon={activePokemon}
-                key={pokemon.id}
-              />
-            );
-          })}
+        {pokemons}
+        {!pokemons.length && !loading && (
+          <Typography>No povemons were found for your request</Typography>
+        )}
       </Styled.PokemonsList>
-      {!!data.length && !filterType && (
+      {!!data.length && (
         <Button
           color="primary"
           variant="contained"
           size="large"
-          sx={{ width: "100%" }}
+          sx={{ width: "100%", marginTop: "auto" }}
           onClick={() => setOffset(data.length)}
         >
           {loading ? (
